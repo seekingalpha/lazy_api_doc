ee1948348005d4e63ee7d134cab5c7ecc35c16bc
Bogdan Guban <bogdan@seekingalpha.com>
2022-05-06 23:54:34 +0300

    Handle ParallelTests (#5)

--- lib/generators/lazy_api_doc/install_generator.rb
+++ lib/generators/lazy_api_doc/install_generator.rb
@@ -1,4 +1,5 @@
 require 'rails/generators'
+require 'lazy_api_doc'
 
 module LazyApiDoc
   module Generators
@@ -7,8 +8,17 @@ module LazyApiDoc
 
       desc "Copy base configuration for LazyApiDoc"
       def install
-        copy_file 'public/index.html', 'public/lazy_api_doc/index.html'
-        copy_file 'public/layout.yml', 'public/lazy_api_doc/layout.yml'
+        copy_file 'public/index.html', "#{LazyApiDoc.path}/index.html"
+        copy_file 'public/layout.yml', "#{LazyApiDoc.path}/layout.yml"
+
+        append_to_file '.gitignore' do
+          <<~TXT
+
+          # LazyApiDoc
+          #{LazyApiDoc.path}/api.yml
+          #{LazyApiDoc.path}/examples/*.json
+          TXT
+        end
 
         install_rspec if Dir.exist?('spec')
 
@@ -21,18 +31,27 @@ module LazyApiDoc
         copy_file 'support/rspec_interceptor.rb', 'spec/support/lazy_api_doc_interceptor.rb'
 
         insert_into_file 'spec/rails_helper.rb', after: "RSpec.configure do |config|\n" do
-          <<~RUBY
-            if ENV['DOC']
-              require 'lazy_api_doc'
-              require 'support/lazy_api_doc_interceptor'
+          <<-RUBY
+  if ENV['LAZY_API_DOC']
+    require 'lazy_api_doc'
+    require 'support/lazy_api_doc_interceptor'
 
-              config.include LazyApiDocInterceptor, type: :request
-              config.include LazyApiDocInterceptor, type: :controller
+    config.include LazyApiDocInterceptor, type: :request
+    config.include LazyApiDocInterceptor, type: :controller
 
-              config.after(:suite) do
-                LazyApiDoc.save_result
-              end
-            end
+    config.after(:suite) do
+      # begin: Handle ParallelTests
+      # This peace of code handle using ParallelTests (tests runs in independent processes).
+      # Just delete this block if you don't use ParallelTests
+      if ENV['TEST_ENV_NUMBER'] && defined?(ParallelTests)
+        LazyApiDoc.save_examples
+        ParallelTests.wait_for_other_processes_to_finish if ParallelTests.first_process?
+        LazyApiDoc.load_examples
+      end
+      # end: Handle ParallelTests
+      LazyApiDoc.generate_documentation
+    end
+  end
           RUBY
         end
       end
@@ -43,7 +62,7 @@ module LazyApiDoc
         append_to_file 'test/test_helper.rb' do
           <<~RUBY
 
-            if ENV['DOC']
+            if ENV['LAZY_API_DOC']
               require 'lazy_api_doc'
               require 'support/lazy_api_doc_interceptor'
 
@@ -52,7 +71,16 @@ module LazyApiDoc
               end
 
               Minitest.after_run do
-                LazyApiDoc.save_result
+                # begin: Handle ParallelTests
+                # This peace of code handle using ParallelTests (tests runs in independent processes).
+                # Just delete this block if you don't use ParallelTests
+                if ENV['TEST_ENV_NUMBER'] && defined?(ParallelTests)
+                  LazyApiDoc.save_examples
+                  ParallelTests.wait_for_other_processes_to_finish if ParallelTests.first_process?
+                  LazyApiDoc.load_examples
+                end
+                # end: Handle ParallelTests
+                LazyApiDoc.generate_documentation
               end
             end
           RUBY
--- lib/lazy_api_doc.rb
+++ lib/lazy_api_doc.rb
@@ -7,60 +7,101 @@ require "yaml"
 module LazyApiDoc
   class Error < StandardError; end
 
-  def self.generator
-    @generator ||= Generator.new
-  end
+  class << self
+    attr_accessor :path, :example_file_ttl
 
-  def self.add(example)
-    generator.add(example)
-  end
+    def configure
+      yield self
+    end
 
-  def self.add_spec(example) # rubocop:disable Metrics/AbcSize
-    add(
-      controller: example.request.params[:controller],
-      action: example.request.params[:action],
-      description: example.class.description,
-      source_location: [example.class.metadata[:file_path], example.class.metadata[:line_number]],
-      verb: example.request.method,
-      params: example.request.params,
-      content_type: example.request.content_type.to_s,
-      request: {
-        query_params: example.request.query_parameters,
-        full_path: example.request.fullpath
-      },
-      response: {
-        code: example.response.status,
-        content_type: example.response.content_type.to_s,
-        body: example.response.body
-      }
-    )
-  end
+    def reset!
+      config_file = './config/lazy_api_doc.yml'
+      config = File.exist?(config_file) ? YAML.safe_load(ERB.new(File.read(config_file)).result) : {}
 
-  def self.add_test(example) # rubocop:disable Metrics/AbcSize
-    add(
-      controller: example.request.params[:controller],
-      action: example.request.params[:action],
-      description: example.name.gsub(/\Atest_/, '').humanize,
-      source_location: example.method(example.name).source_location,
-      verb: example.request.method,
-      params: example.request.params,
-      content_type: example.request.content_type.to_s,
-      request: {
-        query_params: example.request.query_parameters,
-        full_path: example.request.fullpath
-      },
-      response: {
-        code: example.response.status,
-        content_type: example.response.content_type.to_s,
-        body: example.response.body
-      }
-    )
-  end
+      self.path = ENV['LAZY_API_DOC_PATH'] || config['path'] || 'public/lazy_api_doc'
+      self.example_file_ttl = ENV['LAZY_API_DOC_EXAMPLE_FILE_TTL'] || config['example_file_ttl'] || 1800 # 30 minutes
+    end
 
-  def self.save_result(to: 'public/lazy_api_doc/api.yml', layout: 'public/lazy_api_doc/layout.yml')
-    layout = YAML.safe_load(File.read(Rails.root.join(layout)))
-    layout["paths"] ||= {}
-    layout["paths"].merge!(generator.result)
-    File.write(Rails.root.join(to), layout.to_yaml)
+    def generator
+      @generator ||= Generator.new
+    end
+
+    def add(lazy_example)
+      generator.add(lazy_example)
+    end
+
+    def add_spec(rspec_example) # rubocop:disable Metrics/AbcSize
+      add(
+        'controller' => rspec_example.request.params[:controller],
+        'action' => rspec_example.request.params[:action],
+        'description' => rspec_example.class.description,
+        'source_location' => [rspec_example.class.metadata[:file_path], rspec_example.class.metadata[:line_number]],
+        'verb' => rspec_example.request.method,
+        'params' => rspec_example.request.params,
+        'content_type' => rspec_example.request.content_type.to_s,
+        'request' => {
+          'query_params' => rspec_example.request.query_parameters,
+          'full_path' => rspec_example.request.fullpath
+        },
+        'response' => {
+          'code' => rspec_example.response.status,
+          'content_type' => rspec_example.response.content_type.to_s,
+          'body' => rspec_example.response.body
+        }
+      )
+    end
+
+    def add_test(mini_test_example) # rubocop:disable Metrics/AbcSize
+      add(
+        'controller' => mini_test_example.request.params[:controller],
+        'action' => mini_test_example.request.params[:action],
+        'description' => mini_test_example.name.gsub(/\Atest_/, '').humanize,
+        'source_location' => mini_test_example.method(mini_test_example.name).source_location,
+        'verb' => mini_test_example.request.method,
+        'params' => mini_test_example.request.params,
+        'content_type' => mini_test_example.request.content_type.to_s,
+        'request' => {
+          'query_params' => mini_test_example.request.query_parameters,
+          'full_path' => mini_test_example.request.fullpath
+        },
+        'response' => {
+          'code' => mini_test_example.response.status,
+          'content_type' => mini_test_example.response.content_type.to_s,
+          'body' => mini_test_example.response.body
+        }
+      )
+    end
+
+    def generate_documentation
+      layout = YAML.safe_load(File.read("#{path}/layout.yml"))
+      layout["paths"] ||= {}
+      layout["paths"].merge!(generator.result)
+      File.write("#{path}/api.yml", layout.to_yaml)
+    end
+
+    def save_examples
+      FileUtils.mkdir("#{path}/examples") unless File.exist?("#{path}/examples")
+      File.write(
+        "#{path}/examples/rspec_#{ENV['TEST_ENV_NUMBER'] || SecureRandom.uuid}.json",
+        {
+          created_at: Time.now.to_i,
+          examples: generator.examples
+        }.to_json
+      )
+    end
+
+    def load_examples
+      valid_time = Time.now.to_i - example_file_ttl
+      examples = Dir["#{path}/examples/*.json"].flat_map do |file|
+        meta = JSON.parse(File.read(file))
+        next [] if meta['created_at'] < valid_time # do not handle outdated files
+
+        meta['examples']
+      end
+      generator.clear
+      examples.each { |example| add(example) }
+    end
   end
 end
+
+LazyApiDoc.reset!
